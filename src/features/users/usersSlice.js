7c7c41a8321eb2d440790809c74037b3ef4cee36
jehmatthews <jehmatthews30@gmail.com>
2023-04-04 14:10:04 -0700

    add users api to usersSlice and implement feature in postsSlice and AddPostForm to add own posts to form

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,15 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  {id: '0', name: 'Luke Skywalker'},
-  {id: '1', name: 'Frodo Baggins'},
-  {id: '2', name: 'Randy Lahey'}
-]
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await axios.get(USERS_URL);
+  return response.data
+})
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    })
+  }
 })
 
 export const selectAllUsers = (state) => state.users;
